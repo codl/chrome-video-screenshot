19805d0ff4eb0a823e49696e9a5f3d92afff0912
codl <codl@codl.fr>
2016-08-29 18:56:43 +0200

    only create window when the screenshots have been taken

--- inject.js
+++ inject.js
@@ -1,12 +1,17 @@
 (function(){
-    var newwindow = window.open();
     var videos = Array.from(document.querySelectorAll("video"));
+    var canvases = [];
     for(let video of videos){
         let canvas = document.createElement("canvas");
         canvas.width = video.videoWidth;
         canvas.height = video.videoHeight;
         let ctx = canvas.getContext("2d");
         ctx.drawImage(video, 0, 0);
+        canvases.push(canvas);
+    }
+
+    var newwindow = window.open();
+    for(let canvas of canvases){
         newwindow.document.body.appendChild(canvas);
     }
 
